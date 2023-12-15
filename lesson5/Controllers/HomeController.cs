ed8fa10aacf5662a2bc96fa6203140208a4c29ca
aref <arefsolaimany@outlook.com>
2023-12-15 17:03:13 +0330

    read from database

--- lesson5/Controllers/HomeController.cs
+++ lesson5/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using lesson5.Models;
+using System.Data.Common;
 
 namespace lesson5.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly MydbCon db;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,MydbCon mydbCon)
     {
+        db = mydbCon;
         _logger = logger;
     }
 
+//  [HttpPost]
+//     public IActionResult Ourplans (plans plns){
+//         List<plans> Plans = new List<plans>();
+//         plans plan = new plans();
+//         plan.planName = "پلن پایه";
+//         plan.planLimit= 1;
+//         plan.planPrice = 0;
+//         Plans.Add(plan);
+//     }
+
     public IActionResult Index()
     {
-        return View();
+        var qplans = db.plans.ToList(); 
+        return View(qplans);
+
     }
 
     public IActionResult Privacy()
